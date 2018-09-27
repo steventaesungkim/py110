ef14e3f1e45c8de06e70369822e352294b34e6fd
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-09-27 13:50:48 -0400

    working on classes

--- rpg.py
+++ rpg.py
@@ -1,2 +1,13 @@
 #Rewriting using objects
 
+class Characters:
+    def __init__(self, char_name, health, power):
+        self.name = char_name
+        self.health = health
+        self.power = power
+player1 = Characters("Hero")
+player2 = Characters("Goblin")
+
+    def status(self, health, power):
+        self.health = health
+        self.power = power
