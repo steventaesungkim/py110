a062a4faf7bca29f7bce635490ad391690d8d04e
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-09-27 16:17:00 -0400

    buliding rpg game characters using class

--- rpg.py
+++ rpg.py
@@ -1,13 +1,50 @@
-#Rewriting using objects
+# #Rewriting using objects
+
+
+# player1 = Characters("Hero")
+# player2 = Characters("Goblin")
+
+# print(player1.name)
+# print(player2.name)
+
+# print(name.health)
+# print(player1.namepower)
+# print(player2.health)
+# print(player2.power)
+
 
 class Characters:
-    def __init__(self, char_name, health, power):
-        self.name = char_name
+    def __init__(self, name, health, power):
         self.health = health
         self.power = power
-player1 = Characters("Hero")
-player2 = Characters("Goblin")
+    def alive(self, char_health):
+        if char_health.health > 0:
+            return True
+        return False
+   
 
-    def status(self, health, power):
-        self.health = health
-        self.power = power
+
+
+class Hero(Characters):
+    def __init__(self, health, power):
+        self.name = "Hero"
+    def attack(self, enemy):
+        enemy.health -= self.power
+        print("You do %d damage to the Goblin. " % (self.power))
+    def print_status(self):
+        print("You have %d health and %d power. " % (self.health, self.power))
+        
+
+
+class Goblin(Characters):
+    def __init__(self, health, power):
+        self.name = "Enemy"
+    def attack(self, hero):
+        hero.health -= self.power
+        print("You does %d damage to you. "% (self.power))
+    def print_status(self):
+        print("The Goblin has %d health and %d power. " % (self.health, self.power))
+
+
+
+
